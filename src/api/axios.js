0e1de321328a02eac304a6219026c1dd2075eb72
Ry-leigh <ryanaarondc@gmail.com>
2025-10-31 10:14:04 +0800

    Setup: configure Axios instance for API requests with Sanctum

---
+++ src/api/axios.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+// Create a reusable Axios instance
+const axiosClient = axios.create({
+  baseURL: "http://127.0.0.1:8000/api",  // Laravel API endpoint
+  withCredentials: true,                 // allows cookies if using Sanctum
+  headers: {
+    "Accept": "application/json",        // tells Laravel we expect JSON
+    "Content-Type": "application/json",  // sends JSON in request body
+  },
+});
+
+// If a token exists, attach it to all requests
+axiosClient.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
+
+export default axiosClient;
