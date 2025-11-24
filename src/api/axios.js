ce4deb1d8db5b792862260dcdfc175657b3a748d
Ry-leigh <ryanaarondc@gmail.com>
2025-11-25 00:24:45 +0800

    piks name

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
+  // baseURL: "http://127.0.0.1:8000/api",  
   baseURL: "https://astra-backend-qbfe.onrender.com/api",  
   withCredentials: true,                 
   headers: {
